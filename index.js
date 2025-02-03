8db9f061ad3993374bff6180854ba36289a6d370
gahormigag21 <gahormigag@gmail.com>
2025-02-03 18:59:52 -0500

    Implementar gestión de sedes: agregar rutas, controlador y actualizar la base de datos para manejar imágenes grandes

--- index.js
+++ index.js
@@ -8,18 +8,20 @@ const authRoutes = require('./routes/authRoutes');
 const restaurantesRoutes = require('./routes/restaurantes');
 const userRoutes = require('./routes/userRoutes');
 const domicilioRoutes = require('./routes/domicilio');
+const sedesRoutes = require('./routes/sedes');
 
 const app = express();
 
 app.use(cors());
 // Aumentar el límite de tamaño de carga de archivos
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '100mb' })); // Increase the limit to 100mb
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true })); // Increase the limit to 100mb
 
 app.use('/api/domicilio', domicilioRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/restaurantes', restaurantesRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/sedes', sedesRoutes);
 
 app.get('/', (req, res) => {
     res.send('¡Bienvenido al sistema de reservas Géminis!');
