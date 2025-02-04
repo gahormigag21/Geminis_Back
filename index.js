c6903e9a69ced2fb28c36c9c1c932b0f80da7385
gahormigag21 <gahormigag@gmail.com>
2025-02-04 17:46:19 -0500

    Agregar middleware de autenticación y ajustar respuesta de error para token inválido

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const restaurantesRoutes = require('./routes/restaurantes');
 const userRoutes = require('./routes/userRoutes');
 const domicilioRoutes = require('./routes/domicilio');
 const sedesRoutes = require('./routes/sedes');
+const authenticateToken = require('./middlewares/authMiddleware');
 
 const app = express();
 
@@ -17,15 +18,31 @@ app.use(cors());
 app.use(bodyParser.json({ limit: '100mb' })); // Increase the limit to 100mb
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true })); // Increase the limit to 100mb
 
-app.use('/api/domicilio', domicilioRoutes);
+
+
+// Rutas sin autenticación para pruebas
+
 app.use('/api/auth', authRoutes);
+
+app.use('/api/domicilio', domicilioRoutes);
 app.use('/api/restaurantes', restaurantesRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/sedes', sedesRoutes);
-
-app.get('/', (req, res) => {
-    res.send('¡Bienvenido al sistema de reservas Géminis!');
+app.get('/',  (req, res) => {
+    res.send('Usuario autenticado correctamente');
 });
+
+
+
+//Rutas con autenticación
+// app.use('/api/domicilio', authenticateToken, domicilioRoutes);
+// app.use('/api/restaurantes', authenticateToken, restaurantesRoutes);
+// app.use('/api/users', authenticateToken, userRoutes);
+// app.use('/api/sedes', authenticateToken, sedesRoutes);
+// app.get('/', authenticateToken, (req, res) => {
+//     res.send('Usuario autenticado correctamente');
+// });
+
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
